45dfae36aa5eaeacb8aa9607e632e904ef04033d
IlyasValiulov <148232695+IlyasValiulov@users.noreply.github.com>
2025-06-20 14:54:01 +0400

    Задание 10

---
+++ src/test/java/tests/droppable/DroppableTest.java
@@ -0,0 +1,49 @@
+package tests.droppable;
+
+import extensions.ProjectProperties;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.Step;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.droppable.DroppablePage;
+import tests.BaseTest;
+
+import java.io.IOException;
+
+@Epic("Тесты для Protractor")
+@Feature("Тесты страницы входа")
+public class DroppableTest extends BaseTest {
+    private DroppablePage droppablePage;
+    private static final String resourceName = "droppablePage";
+
+    public DroppableTest() throws IOException {
+        super(resourceName);
+    }
+
+    @BeforeMethod
+    @Step("Инициализация страницы")
+    public void setUp() {
+        droppablePage = new DroppablePage(driver);
+    }
+
+    @Test
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Тесты для формы входа")
+    public void dragAndDropTest() throws IOException {
+        SoftAssert softAssert = new SoftAssert();
+        String actualDroppableText = droppablePage.getDroppableElementText();
+        String expectedDroppableText = ProjectProperties.getProperty("expectedDroppableTextBeforeDragAndDrop");
+        softAssert.assertEquals(actualDroppableText, expectedDroppableText);
+
+        droppablePage.dragAndDropDraggableElement();
+        actualDroppableText = droppablePage.getDroppableElementText();
+        expectedDroppableText = ProjectProperties.getProperty("expectedDroppableTextAfterDragAndDrop");
+        softAssert.assertEquals(actualDroppableText, expectedDroppableText);
+        softAssert.assertAll();
+    }
+}
